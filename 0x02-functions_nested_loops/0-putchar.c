ff85ce2d238bcbcd677a2e430c140aea0607c053
Wan Loq <mcjinkz@gmail.com>
2022-09-13 16:25:29 +0100

    Pushed the 0-putchar.c task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+/**
+ * main -  Write a program that prints _putchar, followed by a new line.
+ * Return: 0 on Success
+ */
+#include <stdio.h>
+
+int main()
+{
+    printf("_putchar\n");
+
+    return 0;
+}
