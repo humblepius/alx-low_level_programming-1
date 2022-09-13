7736de449e8c1a7139d022efdf247c78fe0a3d6b
Wan Loq <mcjinkz@gmail.com>
2022-09-13 19:39:34 +0100

    More uploads

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+/**
+ * main -  prints the alphabet, in lowercase, followed by a new line
+ * Return: 0 on Success
+ */
+#include "main.h"
+
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,5 @@
 # Functions and Nested Loops readme
 
 This is my Readme file for this project and i intend to keep it updated
+0   Write a program that prints \_putchar, followed by a new line.
+1   
