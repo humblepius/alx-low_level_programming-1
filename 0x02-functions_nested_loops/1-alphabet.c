90e63ed88ca6d6bfd479039aac5601d39da8f265
Wan Loq <mcjinkz@gmail.com>
2022-09-14 00:02:27 +0100

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,7 @@
-/**
- * print_alphabet -  prints the alphabet, in lowercase, followed by a new line
- * Return: 0 on Success
- */
-
 #include "main.h"
 
 /**
- * print_alphabet - function to print lower case alphabet
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  */
 void print_alphabet(void)
 {
