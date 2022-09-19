961e8ff3545ed3dbda9915e8269c0f9376224247
Wan Loq <mcjinkz@gmail.com>
2022-09-19 21:51:27 +0100

    Initial commit, 0x05

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,7 +2,7 @@
 
 /**
  * swap_int - swaps the values of two integers
- * @a: pointer to first value
+ * @a: pointe r to first value
  * @b: pointer to second value
  */
 void swap_int(int *a, int *b)
