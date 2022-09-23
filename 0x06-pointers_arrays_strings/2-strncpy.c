3ae8ea533125b0b0ee3e256fc2dbcc77c2d00b7b
Michael Jinks <67445420+WanLoq@users.noreply.github.com>
2022-09-23 04:30:56 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,7 +6,7 @@
  * @src: source string
  * @n: number of bytes to copy
  *
- * Return: pointer to the resulting string
+ * Return: pointer to the resulting string.
  */
 char *_strncpy(char *dest, char *src, int n)
 {
