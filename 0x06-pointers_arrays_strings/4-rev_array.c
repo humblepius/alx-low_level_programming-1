7d7526178eb79870303cdde2d6cd58ce652da36d
Michael Jinks <67445420+WanLoq@users.noreply.github.com>
2022-09-23 04:32:13 +0100

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,8 +2,8 @@
 
 /**
  * reverse_array - reverses an array of integers
- * @a: array to be reversed
- * @n: number of elements in the array
+ * @a: array to be reversed.
+ * @n: number of elements in the array.
  */
 void reverse_array(int *a, int n)
 {
