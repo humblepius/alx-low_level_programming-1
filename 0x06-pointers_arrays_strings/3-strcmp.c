a4f7235c62dfa6b2e5e8a6ed9d5bd6b662bc64c7
Michael Jinks <67445420+WanLoq@users.noreply.github.com>
2022-09-23 04:31:41 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,7 @@
  * @s2: second string to compare
  *
  * Return: less than 0 if s1 is less than s2, 0 if they're equal,
- * more than 0 if s1 is greater than s2
+ * more than 0 if s1 is greater than s2.
  */
 int _strcmp(char *s1, char *s2)
 {
